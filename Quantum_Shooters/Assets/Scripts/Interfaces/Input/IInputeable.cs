5b02df8944c1a2e3c7566dbef1fcfef2eb5dbc62
MilenaRPotes <110207483+MilenaRPotes@users.noreply.github.com>
2025-07-31 14:57:22 -0500

    Implemented shooting system and input architecture

---
+++ Quantum_Shooters/Assets/Scripts/Interfaces/Input/IInputeable.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+public interface IInputeable 
+{
+    void ShotPressed();
+    void GetDirection(Vector3 direction);
+}
+   
--- Quantum_Shooters/Assets/Scripts/PlayerController.cs
+++ Quantum_Shooters/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static InputProvider;
 
 [System.Serializable]
 public class Boundary
@@ -12,14 +13,29 @@ public class PlayerController : MonoBehaviour
     public Mover moverComponent;
     public float speed;
     public Boundary boundary;
-    
+
+    public Transform shootOrigin;
+    public GameObject shootPrefab;
+
+    private void Start()
+    {
+        moverComponent.speed = speed;
+        InputProvider.OnHasShoot += OnHasShoot;
+        InputProvider.OnDirection += OnDirection;
+    }
+
+    private void OnDirection(Vector3 direction)
+    {
+        moverComponent.direction = direction;
+    }
+
+    private void OnHasShoot() 
+    {
+        Instantiate(shootPrefab, shootOrigin, false);
+    }
+
     void Update()
     {
-        Vector3 displacement = new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, Input.GetAxis("Vertical") * speed * Time.deltaTime, transform.position.z);
-        moverComponent.DoMove(displacement);
-
-        //X:7.9
-        //Y: 3.8 
         float x = Mathf.Clamp(transform.position.x, boundary.xMinimum, boundary.xMaximum);
         float y = Mathf.Clamp(transform.position.y, boundary.yMinimum, boundary.yMaximum);
         transform.position = new Vector3(x, y);
